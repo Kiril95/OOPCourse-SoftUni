e313939a14211cd56db4dd313c508906923286e7
Kiril95 <tolevkiril@gmail.com>
2021-10-19 23:27:38 +0300

    Adding all exercises in one assembly

---
+++ Encapsulation - Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //This one project assembles four separate exercises from the Encapsulation lecture.
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+
+                    var firstName = cmdArgs[0];
+                    var lastName = cmdArgs[1];
+                    var age = int.Parse(cmdArgs[2]);
+                    var salary = decimal.Parse(cmdArgs[3]);
+
+                    var person = new Person(firstName, lastName, age, salary);
+
+                    persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+            Console.WriteLine(team);
+        }
+    }
+}
---
+++ Encapsulation - Lab/PersonsInfo/Team.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name { get; set; }
+        public IReadOnlyList<Person> FirstTeam => this.firstTeam.AsReadOnly();
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam.AsReadOnly();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+              sb.AppendLine($"First team has {FirstTeam.Count} players.")
+                .AppendLine($"Reserve team has {ReserveTeam.Count} players.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
